8e32280d7a9210bcd0d7875bd087e6aa44388faf
Mario Sánchez <mr.mario.sanchez@gmail.com>
2020-05-26 20:05:00 +0200

    first example generated with canvas-sketch

---
+++ sketch.js
@@ -0,0 +1,85 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require("three");
+
+// Include any additional ThreeJS examples below
+require("three/examples/js/controls/OrbitControls");
+
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  // Dimension settings
+  // dimensions: 'letter',
+  // dimensions: [512, 512],
+  // units: "in" (cm),
+  // sacaleToView: true,
+  // dimensions: 'A4', // if missing is full screen
+  // orientation: 'landscape',
+  // pixelsPerInch: 72,
+  // pixelsPerInch: 300,
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: "webgl"
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas
+  });
+
+  // WebGL background color
+  renderer.setClearColor("black", 1);
+
+  // Setup a camera
+  const camera = new THREE.PerspectiveCamera(
+    50, // distance
+    1, // aspect ratio
+    0.01, // near clipping
+    100 // far clipping
+  );
+  camera.position.set(2, 2, -4);
+  camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  // Setup a geometry
+  const geometry = new THREE.SphereGeometry(1, 32, 16);
+
+  // Setup a material
+  const material = new THREE.MeshBasicMaterial({
+    color: "yellow",
+    wireframe: true
+  });
+
+  // Setup a mesh with geometry + material
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      controls.update();
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      controls.dispose();
+      renderer.dispose();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
